6fac1b1d633eeeab33d5889a9bf93a09e6b7e6a2
EmreUlgut <71984348+EmreUlgut@users.noreply.github.com>
2021-05-08 17:16:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
